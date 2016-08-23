1590926b5cc581c63f4d74f084437c38885b46bc
alexandroAlves <alexandro.r.alves@gmail.com>
2016-08-23 16:09:21 -0300

    Criação de repositório do angular2
    Criação de repositório e adição de dos projetos - 1. HelloWorld, 2.Meu
    primeiro componente

---
+++ 02-primeiro-componete/app/primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-componente' 
+    template: '<h2>Meu primeiro componente Angular 2</h2>'
+
+})
+
+export class MeuPrimeiroComponent {}
