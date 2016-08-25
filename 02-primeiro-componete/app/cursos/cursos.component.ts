7bf23b97f6a59458dfeda8cd805868432d899c20
alexandroAlves <alexandro.r.alves@gmail.com>
2016-08-25 00:09:56 -0300

    Aula 6 - Criação de template

--- 02-primeiro-componete/app/cursos/cursos.component.ts
+++ 02-primeiro-componete/app/cursos/cursos.component.ts
@@ -1,3 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'cursos-lista',
+    /*template: `
+        <h3>Cursos {{ nomePortal }}</h3>
+        <ul>
+            <li *ngFor="let curso of cursos">
+                {{ curso }}
+            </li>
+        </ul>
+    `,*/
+    templateUrl: 'cursos.component.html',
+        
+})
 export class CursosComponent{
+
+    nomePortal = 'Alexandro.traning';
+
+    cursos = ['Angular 2', 'Java', 'Ext JS'];
 
 }
